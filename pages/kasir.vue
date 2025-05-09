cf58ff0fa998525cae838ae2d8ea882c98673529
Aditya Thok <aditya0thok@gmail.com>
2025-05-10 01:08:58 +0700

    feat: layouting kasir

--- pages/kasir.vue
+++ pages/kasir.vue
@@ -6,17 +6,20 @@
     <Card class="md:flex-1">
       <template #content>
         <div class="mb-5">
-          <div class="grid grids-col-2">
-            <div class="col-span-6">
+          <div class="grid grid-cols-4 gap-4">
+            <div class="col-span-2 md:col-span-1">
               <label class="text-sm mb-1">Tanggal</label>
               <DatePicker
                 label="Tanggal"
                 placeholder="Tanggal"
                 v-model="form.tanggal"
                 class="w-full"
+                dateFormat="yy-mm-dd"
+                showTime hourFormat="24"
+                disabled
               />
             </div>
-            <div class="col-span-6">
+            <div class="col-span-4 md:col-span-3">
               <label class="text-sm mb-1">Tagihan</label>
               <FormSelectSiswa :user_id="form.user_id" @selected="onSiswaSelect" @clear="onSiswaSelectClear"/>
             </div>
@@ -27,8 +30,8 @@
           <div class="border p-3 rounded-xl mb-4"> 
             <div class="font-bold mb-1">Tambah Item</div>
             <form @submit.prevent="handleAddItemsTransaksi">
-              <div class="grid grid-cols-6 gap-2">
-                <div class="col-span-6">
+              <div class="flex flex-col md:flex-row gap-4 md:items-end">
+                <div class="md:basis-[40%]">
                   <label class="text-sm mb-1">Nama item</label>
                   <InputText
                     label="Nama Item"
@@ -37,8 +40,8 @@
                     class="w-full"
                   />
                 </div>
-                <div class="col-span-2">
-                  <label class="text-sm mb-1">Harga Item</label>
+                <div class="md:basis-[20%]">
+                  <label class="text-sm mb-1">Harga</label>
                   <InputNumber
                     label="Nominal Item"
                     placeholder="Nominal Item"
@@ -46,37 +49,32 @@
                     class="w-full"
                   />
                 </div>
-                <div class="col-span-2">
+                <div class="md:basis-[20%]">
                   <label class="text-sm mb-1">Qty</label>
-                  <InputNumber
+                  <InputText
+                    type="number"
                     label="Jumlah"
                     placeholder="Jumlah"
                     v-model="formAddItemsTransaksi.jumlah"
                     class="w-full"
                   />
                 </div>
-                <div class="col-span-2">
-                  <label class="text-sm mb-1">Total Harga</label>
-                  <InputNumber
-                    label="Harga"
-                    placeholder="Harga"
-                    v-model="formAddItemsTransaksi.nominal"
-                    class="w-full"
-                    disabled
-                  />
-                </div>
-                <div class="col-span-6 text-end">
-                  <Button type="submit" class="mb-2">Tambah</Button>
+                <div class="md:flex-1">
+                  <Button type="submit" class="w-full">Tambah</Button>
                 </div>
               </div>
             </form>
           </div>
 
-          <div v-if="itemsTransaksi && itemsTransaksi.length > 0" class="border p-3 rounded-xl mb-4">
+          <div class="border p-3 rounded-xl mb-4">
             <div class="font-bold mb-1">Item Transaksi</div>
-            <DataTable :value="itemsTransaksi">
+            <DataTable v-if="itemsTransaksi && itemsTransaksi.length > 0" :value="itemsTransaksi">
               <Column field="nama" header="Item" />
-              <Column field="nominal_item" header="Nominal Item" />
+              <Column field="nominal_item" header="Nominal Item">
+                <template #body="slotProps"> 
+                  {{ formatUang(slotProps.data.nominal_item) }}
+                </template>
+              </Column>
               <Column field="jumlah" header="Qty" />
               <Column field="nominal" header="Total">
                 <template #body="slotProps"> 
@@ -92,7 +90,7 @@
       </template>
     </Card>
     
-    <Card class="md:basis-[20em]">
+    <Card class="md:basis-[30em] lg:basis-[20em]">
       <template #content>
         <div class="mb-3">
           <div>Rekening Tujuan</div>
@@ -102,13 +100,16 @@
           <div>Metode Pembayaran</div>
           <Select v-model="form.metode_pembayaran" :options="[{value:'cash',label:'CASH'}]" optionValue="value" optionLabel="label" class="w-full" />
         </div>
-        <div class="mb-9">
+        <div class="mt-7 mb-9 border-y py-5 text-end">
           <div>Total Pembayaran</div>
-          <div class="text-2xl md:text-4xl font-bold">
+          <div class="text-3xl md:text-4xl font-bold">
             Rp {{ formatUang(totalBayar) }}
           </div>
         </div>
-        <Button type="button" severity="success" class="w-full">Proses Transaksi</Button>
+        <div class="text-end mb-2 text-xs">
+          <Checkbox v-model="form.cetak" size="small" binary /> <label for="cetak">Cetak Bukti</label>
+        </div>
+        <Button @click="prosesTransaksi" type="button" severity="success" class="w-full">Proses Transaksi</Button>
       </template>
     </Card>
 
@@ -157,12 +158,13 @@ async function getTagihans() {
 const form = ref({
   user_id: '',
   siswa_id: '',
-  akun_rekening_id: 'cash',
+  akun_rekening_id: 'CASH',
   metode_pembayaran: 'cash',
   keterangan: '',
-  tanggal: dayjs().utc().local().format('YYYY-MM-DD HH:mm'),
+  tanggal: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+  cetak: true,
   items: [],
-})
+} as any)
 
 function onSiswaSelect(selected: { id: any; user_id: any; }) {
   if(selected.user_id) {
@@ -184,16 +186,14 @@ const itemsTransaksi = ref<any[]>([]) // Inisialisasi sebagai array kosong
 const formAddItemsTransaksi = reactive({
   nama: '',
   deskripsi: '',
-  nominal: 0,
   nominal_item: 0,
   jumlah: 1,
-})
+  nominal: 0,
+} as any)
 
 //watch formAddItemsTransaksi.nominal_item
-watch(() => formAddItemsTransaksi.nominal_item, (newValue) => {
-  if (newValue !== null) {
-    formAddItemsTransaksi.nominal = newValue * formAddItemsTransaksi.jumlah
-  }
+watch(() => [formAddItemsTransaksi.nominal_item,formAddItemsTransaksi.jumlah], () => {
+  formAddItemsTransaksi.nominal = formAddItemsTransaksi.nominal_item * formAddItemsTransaksi.jumlah
 })
 
 const resetForm = () => {
@@ -227,4 +227,25 @@ const hapusItemTransaksi = (index: number) => {
 const totalBayar = computed(() => {
   return itemsTransaksi.value.reduce((total, item) => total + item.nominal, 0)
 })
+
+//proses transaksi
+const loadingProsesTransaksi = ref(false)
+const errorProsesTransaksi = ref({} as any)
+async function prosesTransaksi() {
+  loadingProsesTransaksi.value = true
+  errorProsesTransaksi.value = {}
+  try {
+    const res = await client('/api/transaksi', {
+      method: 'POST',
+      params: {
+        ...form.value,
+       items: itemsTransaksi.value,
+      }
+    })
+  } catch (error) {
+    const er = useSanctumError(error)
+    errorProsesTransaksi.value = er.bag
+  }
+  loadingProsesTransaksi.value = false
+}
 </script>
