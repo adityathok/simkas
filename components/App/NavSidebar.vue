12caec073dbf7a237198046119f97fff24c3650f
Aditya Thok <aditya0thok@gmail.com>
2025-05-16 00:28:23 +0700

    refactor: sidebar menu from config

--- components/App/NavSidebar.vue
+++ components/App/NavSidebar.vue
@@ -4,25 +4,25 @@
     <div class="py-6 px-2 flex justify-center brand-logo overflow-hidden">
        <AppLogo />    
     </div>
-
     <ScrollPanel style="width: 100%; height: calc(100vh - 7rem)">
         <PanelMenu
-        :model="items"
-        class="w-full border-none rounded-none gap-0!"
-        :pt="{
-            panel: (options) => ({
-                class: [
-                    'border-none!',
-                    'bg-transparent!'
-                ]
-            }),
-            rootList: (options) => ({
-                class: [
-                    'ps-0!',
-                ]
-            })
-        }"
-        v-model:expandedKeys="expandedKeys"
+            v-if="items"
+            :model="items"
+            class="w-full border-none rounded-none gap-0!"
+            :pt="{
+                panel: (options) => ({
+                    class: [
+                        'border-none!',
+                        'bg-transparent!'
+                    ]
+                }),
+                rootList: (options) => ({
+                    class: [
+                        'ps-0!',
+                    ]
+                })
+            }"
+            v-model:expandedKeys="expandedKeys"
         >
         <template #item="{ item }">
 
@@ -46,6 +46,16 @@
                                     
             </template>
         </PanelMenu>
+
+        <div v-else>
+            <template v-for="i in 8">
+                <div class="flex mb-2 px-2">
+                    <Skeleton width="2.5rem" height="2rem" class="mr-3"/>
+                    <Skeleton height="2rem"/>
+                </div>
+            </template>
+        </div>
+
     </ScrollPanel>
     
   </div>
@@ -54,6 +64,7 @@
 <script setup lang="ts">
 
 const useGlobal = useGlobalStore()
+const { config } = useConfigStore()
 
 //route, cek halaman aktif
 const route = useRoute()
@@ -104,190 +115,8 @@ watch(() => expandedKeys, () => {
 const classLink = 'w-full flex items-center justify-start px-2.5 py-2.5 mb-0.5 gap-3 text-start leading-[normal] font-normal hover:bg-blue-100 hover:text-blue-800 text-link bg-transparent group/link cursor-pointer';
 
 //daftar menu
-const items = ref([
-    {
-        key: 'dashboard',
-        label: 'Dashboard',
-        icon: 'lucide:layout-grid',
-        route:'/',
-    },
-    {
-        key: 'kasir',
-        label: 'Kasir',
-        icon: 'lucide:monitor-dot',
-        route:'/kasir',
-    },
-    {
-        key: 'transaksi',
-        label: 'Transaksi',
-        icon: 'lucide:coins',
-        items:[
-            {
-                key: 'riwayat_transaksi',
-                label: 'Riwayat Transaksi',
-                route:'/transaksi',
-            },
-            {
-                key: 'create_transaksi',
-                label: 'Tambah Transaksi',
-                route:'/transaksi/create',
-            },
-        ]
-    },
-    {
-        key: 'tagihan',
-        label: 'Tagihan',
-        icon: 'lucide:receipt-text',
-        items:[
-            {
-                key: 'riwayat_tagihannaster',
-                label: 'Master Tagihan',
-                route:'/tagihanmaster',
-            },
-            {
-                key: 'riwayat_tagihan',
-                label: 'Semua Tagihan',
-                route:'/tagihan',
-            },
-            {
-                key: 'create_tagihan',
-                label: 'Buat Tagihan',
-                route:'/tagihan/create',
-            },
-        ]
-    },
-    {
-        key: 'akuntansi',
-        label: 'Akuntansi',
-        icon: 'lucide:calculator',
-        items: [
-            {
-                key: 'akun_pendapatan',
-                label: 'Akun Pendapatan',
-                route:'/akunpendapatan',
-            },
-            {
-                key: 'akun_pengeluaran',
-                label: 'Akun Biaya',
-                route:'/akunpengeluaran',
-            },
-            {
-                key: 'jurnlkas',
-                label: 'Jurnal',
-                route:'/jurnalkas',
-            },
-            {
-                key: 'akun_rekening',
-                label: 'Akun Rekening',
-                route:'/akunrekening',
-            },
-        ]
-    },
-    {
-        key: 'siswa',
-        label: 'Siswa',
-        icon: 'lucide:users',
-        items: [
-            {
-                key: 'siswa_data',
-                label: 'Data Siswa',
-                route:'/siswa',
-            },
-            {
-                key: 'siswa_tambah',
-                label: 'Tambah Siswa',
-                route:'/siswa/create',
-            },
-            {
-                key: 'siswa_naik',
-                label: 'Naik Kelas',
-                route:'/siswa/naikkelas',
-            },
-            {
-                key: 'siswa_import',
-                label: 'Import Siswa',
-                route:'/siswa/import',
-            }
-        ]
-    },
-    {
-        key: 'unit',
-        label: 'Unit Sekolah',
-        icon: 'lucide:school',
-        items: [
-            {
-                key: 'unit_create',
-                label: 'Semua Unit',
-                route:'/unitsekolah',
-            },
-            {
-                key: 'unit_kelas',
-                label: 'Kelas',
-                route:'/kelas',
-            },
-        ]
-    },
-    {
-        key: 'pegawai',
-        label: 'Pegawai',
-        icon: 'lucide:circle-user',
-        items: [
-            {
-                key: 'pegawai_all',
-                label: 'Semua Pegawai',
-                route:'/pegawai',
-            },
-            {
-                key: 'pegawai_add',
-                label: 'Tambah Pegawai',
-                route:'/pegawai/add',
-            },
-        ]
-    },
-    {
-        key: 'pengaturan',
-        label: 'Pengaturan',
-        icon: 'lucide:settings',
-        // route:'/pengaturan',
-        items: [
-            {
-                key: 'pengaturan_lembaga',
-                label: 'Lembaga',
-                route:'/pengaturan',
-            },
-            {
-                key: 'pengaturan_tahunajaran',
-                label: 'Tahun Ajaran',
-                route:'/pengaturan/tahunajaran',
-            },
-            {
-                key: 'pengaturan_jenjang',
-                label: 'Jenjang',
-                route:'/pengaturan/jenjang',
-            },
-            {
-                key: 'pengaturan_jabatan',
-                label: 'Jabatan',
-                route:'/pengaturan/jabatan',
-            },
-        ]
-    },
-    {
-        key: 'users',
-        label: 'Users',
-        icon: 'lucide:square-user',
-        items: [
-            {
-                key: 'users_all',
-                label: 'Semua User',
-                route:'/users',
-            },
-            {
-                key: '3_1',
-                label: 'Tambah User',
-                route:'/users/create',
-            }
-        ]
-    },
-]);
+const items = computed(() => {
+    return config.app_menus
+})
+
 </script>
