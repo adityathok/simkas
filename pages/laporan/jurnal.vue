8bdf7dd905655f5885eec6cba2bf4f7b5a7f272f
Aditya Thok <aditya0thok@gmail.com>
2025-05-25 22:28:28 +0700

    feat: jurnal laporan controller

--- pages/laporan/jurnal.vue
+++ pages/laporan/jurnal.vue
@@ -1,16 +1,55 @@
 <template>
   <div class="mb-5">
-    <DatePicker v-model="filter.tanggal" dateFormat="mm/yy/dd" :manualInput="true" size="small" />
-    <Button @click="refresh()" size="small">
-      <Icon name="lucide-search" class="mr-1" /> Reload
+    <DatePicker v-model="filter.tanggal" dateFormat="dd/mm/yy" :manualInput="true" size="small" />
+    <Button @click="getData()" size="small">
+      <Icon name="lucide-refresh-cw" class="mr-1" :class="{'animate-spin':loadingData}" /> Reload
     </Button>
   </div>
 
   <DashCard>
 
       <DataTable :value="data.data" class="text-sm text-nowrap" stripedRows scrollable>
-        <Column selectionMode="multiple"></Column>
-        <Column field="tanggal" header="Tgl" /> 
+        <!-- <Column selectionMode="multiple"></Column> -->
+        <Column field="transaksi.tanggal" header="Tgl" />
+        <Column field="rekening_id" header="Rek">        
+          <template #body="slotProps">          
+            <span v-if="slotProps.data.transaksi.akun_rekening_id">  
+              {{ slotProps.data.transaksi.akun_rekening.nama }}
+            </span>
+          </template>
+        </Column>
+        <Column field="deskripsi" header="Deskripsi">          
+          <template #body="slotProps">
+            <div>
+              {{ slotProps.data.nama }}
+            </div>
+          </template>
+        </Column>
+        <Column field="user_id" header="Nama">          
+          <template #body="slotProps">
+            <div>
+              {{ slotProps.data.transaksi.user?.name }}
+            </div>
+            <div v-if="slotProps.data.transaksi.user.siswa" class="text-xs opacity-50">
+                {{ slotProps.data.transaksi.user.siswa?.nis }} / {{ slotProps.data.transaksi.user.siswa?.kelas_siswa.nama }}
+            </div>
+          </template>
+        </Column> 
+        <Column field="debit" header="Debit">          
+          <template #body="slotProps">
+            <div v-if="slotProps.data.akun_pendapatan_id">
+              {{ formatUang(slotProps.data.nominal) }}
+            </div>
+          </template>
+        </Column>
+        <Column field="kredit" header="Kredit">          
+          <template #body="slotProps">
+            <div v-if="slotProps.data.akun_pengeluaran_id">
+              {{ formatUang(slotProps.data.nominal) }}
+            </div>
+          </template>
+        </Column>
+        <!-- <Column field="tanggal" header="Tgl" /> 
         <Column field="user_id" header="Nama">          
           <template #body="slotProps">
             <div>
@@ -27,7 +66,7 @@
               {{ slotProps.data.akun_rekening.nama }}
             </span>
           </template>
-        </Column>
+        </Column> -->
       </DataTable>
        <div class="flex flex-col md:flex-row md:justify-between items-center">
         <div class="text-sm text-slate-400">
@@ -56,6 +95,7 @@
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 dayjs.extend(utc);
+import { formatUang } from '~/utils/formatUang';
 const client = useSanctumClient();
 
 definePageMeta({
@@ -67,17 +107,40 @@ const filter = reactive({
   tanggal: route.query.tanggal || dayjs().utc().local().format('YYYY-MM-DD') as any,
   page: route.query.tanggal || 1,
   per_page : 20,
-  jurnal_id:'',
+  jurnal_id: route.query.id || '',
 } as any)
 
-const { data, status, error, refresh } = await useAsyncData(
-    'jurnal-page-'+filter,
-    () => client('/api/transaksi',{
+const data = ref({} as any)
+const loadingData = ref(false);
+async function getData() {
+  loadingData.value = true;
+
+  //ubah tanggal ke format YYYY-MM-DD
+  filter.tanggal = dayjs(filter.tanggal).format('YYYY-MM-DD');
+
+  try {
+    const res = await client('/api/laporan_jurnal',{
       params : filter
     })
-)
+    data.value = res;
+  } catch (error) {
+    console.log(error);
+  }
+  loadingData.value = false;
+}
+//
+onMounted(() => {
+  getData()
+})
+// const { data, status, error, refresh } = await useAsyncData(
+//     'laporan-jurnal-page-'+filter,
+//     () => client('/api/laporan_jurnal',{
+//       params : filter
+//     })
+// )
 const onPaginate = (event: { page: number }) => {
     filter.page = event.page + 1;
-    refresh()
+    // refresh()
+    getData()
 };
 </script>
