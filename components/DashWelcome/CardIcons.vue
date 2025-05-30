caf82f3bdd8a56af3e7b981b40b2fee39e144699
Aditya Thok <aditya0thok@gmail.com>
2025-05-31 05:35:16 +0700

    feat; dashoard

---
+++ components/DashWelcome/CardIcons.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="overflow-x-auto">
+     <div class="flex justify-between gap-4">
+      
+        <DashCard class="min-w-[200px] md:min-w-[23%] flex justify-between bg-sky-100">
+          <div class="overview">
+            <div class="text-xs">
+              Pendapatan bulan ini
+            </div>
+            <div class="text-2xl">
+              {{ formatUang(props.total_pendapatan)  }}
+            </div>
+          </div>
+          <div>
+            <Icon name="lucide:download" />
+          </div>
+        </DashCard>
+
+        <DashCard class="min-w-[200px] md:min-w-[23%] flex justify-between bg-emerald-100">
+          <div class="overview">
+            <div class="text-xs">
+              Pengeluaran bulan ini
+            </div>
+            <div class="text-2xl">
+              {{ formatUang(props.total_pengeluaran)  }}
+            </div>
+          </div>
+          <div>
+            <Icon name="lucide:upload" />
+          </div>
+        </DashCard>
+
+        <DashCard class="min-w-[200px] md:min-w-[23%] flex justify-between bg-yellow-100">
+          <div class="overview">
+            <div class="text-xs">
+              Transaksi hari ini
+            </div>
+            <div class="text-2xl">
+              250.000.000
+            </div>
+          </div>
+          <div>
+            <Icon name="lucide:activity" />
+          </div>
+        </DashCard>
+
+        <DashCard class="min-w-[200px] md:min-w-[23%] flex justify-between bg-zinc-100">
+          <div class="overview">
+            <div class="text-xs">
+              Tagihan bulanan ini
+            </div>
+            <div class="text-2xl">
+              250.000.000
+            </div>
+          </div>
+          <div>
+            <Icon name="lucide:receipt-text" />
+          </div>
+        </DashCard>
+        
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps(['total_pendapatan','total_pengeluaran'])
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
-    <h1>Welcome to the homepage</h1>
+
+  <div class="mb-10">
+    <DashWelcomeCardIcons :total_pendapatan="data.total_pendapatan" :total_pengeluaran="data.total_pengeluaran"/>   
   </div>
+
+  <div class="flex flex-col md:flex-row gap-4">
+
+    <div class="basis-1 md:basis-[70%]">
+      <DashCard>
+        <DashWelcomeChartLine />
+      </DashCard>
+    </div>
+    <div class="basis-1 md:basis-[30%]">
+      <DashCard>
+      </DashCard>
+    </div>
+
+  </div>
+
+
 </template>
+
+<script setup lang="ts">
+const client = useSanctumClient();
+const { data, refresh } = await useAsyncData(
+    'dashboard',
+    () => client('/api/dashboard')
+)
+</script>
